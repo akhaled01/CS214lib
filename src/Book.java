9c0d7cd3d2df835f7980d8a025441fe22e7feb1e
khadija0404 <202200734@stu.uob.edu.bh>
2023-12-08 19:54:22 +0300

    Khadija: added Book and Libmember

--- src/Book.java
+++ src/Book.java
@@ -1,5 +1,156 @@
 public class Book {
-    // temp additions
-    
-    
+    //this class contains information about the book in the library system
+    private String title;
+    private String author1;
+    private String author2;
+    private String publisher;
+    private int yearPublication;
+    private String isbn;
+    private long accessionNum;
+    private LibMember issuedTo;
+    public Book (){
+        //default constructor initializing each data member to default values
+        title = null;
+        author1 = null;
+        author2 = null;
+        publisher = null;
+        yearPublication = 0;
+        isbn = "0000000000000";
+        accessionNum = 1000;
+        issuedTo = null;
+    }
+    public Book (String t, String a1, String a2, String p, int yP, String ISBN, long aN){
+        //constructor with parameters assigning the passed parameters to each of the data members
+        title = t;
+        author1 = a1;
+        author2 = a2;
+        publisher = p;
+        yearPublication = yP;
+        //checking if the ISBN passed is 13-digits, otherwise setting it to default value
+        if (ISBN.length() == 13) {
+            isbn = ISBN;
+        }
+        else {
+            isbn = "0000000000000";
+        }
+        //checking if the aN passed is more than 1000, otherwise setting it to default value
+        if (aN > 1000) {
+            accessionNum = aN;
+        }
+        else {
+            accessionNum = 1000;
+        }
+        issuedTo = null;
+    }
+    public void setTitle (String t){
+        //set method for title
+        title = t;
+    }
+    public void setAuthor1 (String a1){
+        //set method for author1
+        author1 = a1;
+    }
+    public void setAuthor2 (String a2){
+        //set method for author2
+        author2 = a2;
+    }
+    public void setPublisher (String p){
+        //set method for publisher
+        publisher = p;
+    }
+    public void setYearPublication (int yP){
+        //set method for yearPublication
+        yearPublication = yP;
+    }
+    public void setIsbn (String ISBN){
+        //set method for isbn, checks if ISBN is 13-digits first, otherwise sets it to default value
+        if (ISBN.length() == 13){
+            isbn = ISBN;
+        }
+        else {
+            isbn = "0000000000000";
+        }
+    }
+    public void setAccessionNum (long aN){
+        //set method for accessionNum, checks if aN is more than 1000, otherwise sets it to default value
+        if (aN > 1000){
+            accessionNum = aN;
+        }
+        else {
+            accessionNum = 1000;
+        }
+    }
+    public void setIssuedTo (LibMember iT){
+        //set method for issuedTo
+        issuedTo = iT;
+    }
+    public String getTitle (){
+        //get method to return the title
+        return title;
+    }
+    public String getAuthor1 (){
+        //get method to return author1
+        return author1;
+    }
+    public String getAuthor2 (){
+        //get method to return author2
+        return author2;
+    }
+    public String getPublisher (){
+        //get method to return the publisher
+        return publisher;
+    }
+    public int getYearPublication (){
+        //get method to return the year publication
+        return yearPublication;
+    }
+    public String getIsbn (){
+        //get method to return the isbn
+        return isbn;
+    }
+    public long getAccessionNum (){
+        //get method to return accessionNum
+        return accessionNum;
+    }
+    public LibMember getIssuedTo () {
+        //get method to return who the book is issuedTo
+        return issuedTo;
+    }
+    public boolean equals (Book book){
+        //equals method to see if the passed book equals this book
+        if (this == book){
+            return true;
+        }
+        if (book == null){
+            return false;
+        }
+        if (! this.getTitle().equalsIgnoreCase(book.getTitle())){
+            return false;
+        }
+        else if (! this.getAuthor1().equalsIgnoreCase(book.getAuthor1())){
+            return false;
+        }
+        else if (! this.getAuthor2().equalsIgnoreCase(book.getAuthor2())){
+            return false;
+        }
+        else if (! this.getPublisher().equalsIgnoreCase(book.getPublisher())){
+            return false;
+        }
+        else if (this.getYearPublication()!=book.getYearPublication()){
+            return false;
+        }
+        else if(! this.getIsbn().equalsIgnoreCase(book.getIsbn())){
+            return false;
+        }
+        else if (this.getAccessionNum()!= book.getAccessionNum()){
+            return false;
+        }
+        else if (this.getIssuedTo()!= book.getIssuedTo()){
+            return false;
+        }
+        return true;
+    }
+    public String toString (){
+
+    }
 }
--- src/LibMember.java
+++ src/LibMember.java
@@ -1,4 +1,106 @@
+import java.util.*;
 public class LibMember {
-    
-    
+    private String firstName;
+    private String lastName;
+    private char gender;
+    private long cprNum;
+    private String teleNum;
+    private Book[] booksIssued;
+    private int numBooksIssued;
+    public LibMember(){
+        //default constructor to initialize all the data members to their default values
+        firstName = null;
+        lastName = null;
+        gender = ' ';
+        cprNum = 0;
+        teleNum = null;
+        booksIssued = new Book[10];
+        numBooksIssued = 0;
+    }
+    public LibMember (String fN, String lN, char g, long cN, String tN){
+        //constructor with parameters to initialize all data members to the passed parameters, except the array booksIssued and the number of booksIssued
+        firstName = fN;
+        lastName = lN;
+        gender = g;
+        cprNum = cN;
+        teleNum = tN;
+        booksIssued = new Book [10];
+        numBooksIssued = 0;
+    }
+
+    public void setFirstName (String fN){
+        firstName = fN;
+    }
+    public void setLastName (String lN){
+        lastName = lN;
+    }
+    public void setGender (char g){
+        gender = g;
+    }
+    public void setCprNum (long cN){
+        cprNum = cN;
+    }
+    public void setTeleNum (String tN){
+        teleNum = tN;
+    }
+    public void setBooksIssued (Book[] bI){
+        booksIssued = bI;
+    }
+    public void setNumBooksIssued (int nBI){
+        numBooksIssued = nBI;
+    }
+    public String getFirstName (){
+        return firstName;
+    }
+    public String getLastName (){
+        return lastName;
+    }
+    public char getGender (){
+        return gender;
+    }
+    public long getCprNum (){
+        return cprNum;
+    }
+    public String getTeleNum (){
+        return teleNum;
+    }
+    public Book[] getBooksIssued (){
+        return booksIssued;
+    }
+    public int getNumBooksIssued (){
+        return numBooksIssued;
+    }
+    public boolean equals (LibMember libMember){
+        if (this == libMember){
+            return true;
+        }
+        if (libMember == null){
+            return false;
+        }
+        if (! this.getFirstName().equalsIgnoreCase(libMember.getFirstName())){
+            return false;
+        }
+        else if (! this.getLastName().equalsIgnoreCase(libMember.getLastName())){
+            return false;
+        }
+        else if (this.getGender() != libMember.getGender()){
+            return false;
+        }
+        else if (this.getCprNum() != libMember.getCprNum()){
+            return false;
+        }
+        else if (! this.getTeleNum().equalsIgnoreCase(libMember.getTeleNum())){
+            return false;
+        }
+        else if (! Arrays.toString(this.getBooksIssued()).equalsIgnoreCase(Arrays.toString(libMember.getBooksIssued()))) {
+            return false;
+        }
+        else if (this.getNumBooksIssued()!= libMember.getNumBooksIssued()){
+            return false;
+        }
+        return true;
+    }
+    public String toString(){
+
+    }
 }
